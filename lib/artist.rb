50058de87fbee6b382f8eaacff402e7d29d8ac62
Leah Riffell <leah.riffell@gmail.com>
2020-07-27 22:13:25 -0600

    Add method for returning an artist's age at death

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
   attr_reader :id, :name, :born, :died, :country 
-  
+
   def initialize(attributes)
     @id = attributes[:id]
     @name = attributes[:name]
@@ -8,4 +8,8 @@ class Artist
     @died = attributes[:died]
     @country = attributes[:country]
   end 
+
+  def age_at_death 
+    @died.to_i - @born.to_i
+  end
 end
