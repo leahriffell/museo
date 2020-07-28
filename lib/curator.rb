4ecf17ec66e075952665c794f094e04c4b240d7e
Leah Riffell <leah.riffell@gmail.com>
2020-07-27 22:26:17 -0600

    Add method for adding artists to a curator

--- lib/curator.rb
+++ lib/curator.rb
@@ -4,13 +4,18 @@ require './lib/file_io'
 require 'pry'
 
 class Curator 
-  attr_reader :photographs 
+  attr_reader :photographs, :artists
 
   def initialize 
     @photographs = [] 
+    @artists = [] 
   end
 
   def add_photograph(photo)
     @photographs << photo
+  end
+
+  def add_artist(artist)
+    @artists << artist
   end
 end
