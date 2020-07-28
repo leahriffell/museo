de7b61070a2e2cb09e30cbe6ab402719d58c547c
Leah Riffell <leah.riffell@gmail.com>
2020-07-27 21:45:03 -0600

    Add initial tests for photograph class

--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -2,7 +2,24 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/photograph'
+require './lib/artist'
 
 class PhotographTest < Minitest::Test
+  def setup 
+    attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "4",
+      year: "1954"
+    }
+    @photograph = Photograph.new(attributes)
+  end
 
+  def test_it_exists_and_has_readable_attributes 
+    assert_instance_of Photograph, @photograph 
+    assert_equal "1", @photograph.id 
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", @photograph.name
+    assert_equal "4", @photograph.artist_id
+    assert_equal "1954", @photograph.year
+  end
 end
