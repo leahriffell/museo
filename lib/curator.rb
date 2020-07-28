da7950ac1666c5ec959c0f935d84478d98ea05b4
Leah Riffell <leah.riffell@gmail.com>
2020-07-27 22:22:42 -0600

    Add method for adding photographs to a curator's photograph collection

--- lib/curator.rb
+++ lib/curator.rb
@@ -4,4 +4,13 @@ require './lib/file_io'
 require 'pry'
 
 class Curator 
+  attr_reader :photographs 
+
+  def initialize 
+    @photographs = [] 
+  end
+
+  def add_photograph(photo)
+    @photographs << photo
+  end
 end
