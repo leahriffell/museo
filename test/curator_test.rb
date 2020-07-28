7678e6ab8de4151d96bc1fa3c5dc972010e6ac3d
Leah Riffell <leah.riffell@gmail.com>
2020-07-27 22:18:06 -0600

    Add initial tests for Curator class

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -6,5 +6,23 @@ require './lib/photograph'
 require './lib/artist'
 
 class CuratorTest < Minitest::Test
-  
+  def setup 
+    @curator = Curator.new
+    @photo_1 = Photograph.new({
+      id: "1",      
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+      artist_id: "1",      
+      year: "1954"      
+    })
+    @photo_2 = Photograph.new({
+      id: "2",      
+      name: "Moonrise, Hernandez",      
+      artist_id: "2",      
+      year: "1941"      
+    })           
+  end
+
+  def test_it_exists 
+    assert_instance_of Curator, @curator
+  end
 end
